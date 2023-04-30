16970bd7cd516b0d107c70476be21e6dd56b5bd5
Erza Fahmi Fasya <58721335+erzaffasya@users.noreply.github.com>
2023-05-01 07:23:30 +0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -11,17 +11,9 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
-    host: process.env.DB_HOST,
-    port: 5432,
-    dialect : 'postgres'  
-  });
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: process.env.DB_HOST,
-    port: 5432,
-    dialect : 'postgres'
-  });
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
